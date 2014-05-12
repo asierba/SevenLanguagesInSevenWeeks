4361cd396ac4a8260a76e7059a58e5701abf2c99
Asier Barrenetxea <asierba@gmail.com>
2014-05-12 23:42:17 +0100

    Starte with Day1

---
+++ Scala/Day1/game.scala
@@ -0,0 +1,18 @@
+val tictactoe = Array(
+									Array('X', 'X', 'X'),
+									Array(' ', ' ', ' '),
+									Array(' ', ' ', ' '))
+
+def printStatus(board: Array[Array[Char]]) {
+	if(true) {
+		println("X wins!")
+	} else if(false) {
+		println("O wins!")
+	} else if(true) {
+		println("Tie!")
+	} else {
+		println("no winner yet..")
+	}
+}
+
+printStatus(tictactoe)
