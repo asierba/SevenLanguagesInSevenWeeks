e384b1a6fc402cfffea31a1cefbecc407755c90e
Asier Barrenetxea <1537266+asierba@users.noreply.github.com>
2014-05-22 23:49:51 +0100

    First row checks for X winner

--- Scala/Day1/src/test/scala/TicTacToe.scala
+++ Scala/Day1/src/test/scala/TicTacToe.scala
@@ -2,11 +2,20 @@ import org.scalatest._
 
 class TicTacToe extends FunSpec with ShouldMatchers {
 
-  def getWinner(x : Array[Array[Char]]) = {
-    ' '
+  def getWinner(board : Array[Array[Char]]) : Char = {
+    val user = 'X'
+    
+    for( x <- board(0)) {   
+      if (x != user) {
+        return ' '
+      }
+        
+    }
+    
+    return user
   }
 
-  describe("In an empty board") {
+  describe("when the board is empty") {
     it("there should be no winner") {
       val board = Array(
 		    Array(' ', ' ', ' '),
@@ -16,4 +25,49 @@ class TicTacToe extends FunSpec with ShouldMatchers {
       	getWinner(board) should be (' ')
     }
   }
+
+  describe("when the game is not over yet") {
+    it("there should be no winner") {
+      val board = Array(
+        Array('X', 'O', 'X'),
+        Array(' ', 'O', ' '),
+        Array(' ', 'X', 'O'))
+
+        getWinner(board) should be (' ')
+    }
+  }
+
+  describe("when the game is not over yet v2") {
+    it("there should be no winner") {
+      val board = Array(
+        Array(' ', 'X', 'X'),
+        Array(' ', ' ', ' '),
+        Array(' ', ' ', ' '))
+
+        getWinner(board) should be (' ')
+    }
+  }
+
+  describe("when the board is empty v3") {
+    it("there should be no winner") {
+      val board = Array(
+        Array('X', 'X', ' '),
+        Array(' ', ' ', ' '),
+        Array(' ', ' ', ' '))
+
+        getWinner(board) should be (' ')
+    }
+  }
+
+  describe("when X makes three in a row") {
+    it("X should be the winner") {
+      val board = Array(
+        Array('X', 'X', 'X'),
+        Array(' ', 'O', ' '),
+        Array(' ', 'O', 'O'))
+
+        getWinner(board) should be ('X')
+    }
+  }
+
 }
